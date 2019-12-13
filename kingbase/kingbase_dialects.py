63bd6f062a23ae1871ffc8adf18ed8c9cde02779
wangmin <wangmin@eist.cn>
2019-12-13 11:27:31 +0800

    refactor postgres drivers for sqlalchemy

--- kingbase/kingbase_dialects.py
+++ kingbase/kingbase_dialects.py
@@ -220,7 +220,7 @@ class PGDialect_kingbase(PGDialect_psycopg2):
         ) in rows:
             column_info = self._get_column_info(
                 name,
-                format_type.upper(),
+                format_type.lower(),
                 default_,
                 notnull,
                 domains,
