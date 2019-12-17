88a17cb518723ac3963c100de4d27b88dce257da
wangmin <wangmin@eist.cn>
2019-12-17 15:05:33 +0800

    remove other file head

--- kingbase/__init__.py
+++ kingbase/__init__.py
@@ -1,8 +1,1 @@
-# postgresql/__init__.py
-# Copyright (C) 2005-2019 the SQLAlchemy authors and contributors
-# <see AUTHORS file>
-#
-# This module is part of SQLAlchemy and is released under
-# the MIT License: http://www.opensource.org/licenses/mit-license.php
-
 __version__ = "0.0.1"
